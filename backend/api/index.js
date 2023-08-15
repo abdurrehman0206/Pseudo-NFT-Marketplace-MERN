44937bccd3b8938b6a26bf2b1837481f9e2d341d
abdurrehman0206 <abdurrehmansaleem5@gmail.com>
2023-08-15 18:35:05 +0500

    Update

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,39 @@
 const app = require("../index");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const serverless = require("serverless-http");
+const corsOpts = {
+  origin: ["http://localhost:3000", "http://192.168.100.29:3000"],
+  credentials: true,
+  methods: ["GET", "POST", "HEAD", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  exposedHeaders: ["Content-Type", "Authorization"],
+};
 
+app.use(cors(corsOpts));
+app.use(express.json());
+const logger = (req, res, next) => {
+  console.log(req.method, req.url, res.statusCode);
+  next();
+};
+
+app.use(logger);
+
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running at ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+    throw new Error(err.message);
+  });
 module.exports = app;
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-require("dotenv").config();
 const mongoose = require("mongoose");
+require("dotenv").config();
 const cors = require("cors");
 const app = express();
 const serverless = require("serverless-http");
